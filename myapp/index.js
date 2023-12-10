df7be9f3fe514f20ffa73d9de8140df5d4ca9f88
Dasaradhakiran <kirankadali33344@gmail.com>
2023-12-10 19:31:34 +0530

    new commit

--- myapp/index.js
+++ myapp/index.js
@@ -2,9 +2,10 @@ const express = require("express");
 const path = require("path");
 const {open} = require("sqlite");
 const sqlite3 = require("sqlite3");
+const cors = require("cors");
 
 const app = express();
-app.use(express.json());
+app.use(cors());
 
 const dbPath = path.join(__dirname,"roxilerdatabase.db");
 
